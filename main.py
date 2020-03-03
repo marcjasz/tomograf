4126aa468672cdecfad4b1b2e29f5cab91798d2e
Marcin Jachymski <marcin.jachymski@bitgamelabs.com>
2020-03-03 21:20:35 +0100

    pad images to square shape

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from skimage import io, color
+from skimage import io, color, util
 from matplotlib import pyplot as plt
+import math
+
+def to_square(image):
+    diff = image.shape[0] - image.shape[1]
+    padding = (0,)
+    if diff > 0:
+        padding = ((0, 0), (diff//2, diff - diff//2))
+    elif diff < 0:
+        padding = ((-math.ceil(diff/2), - diff + math.ceil(diff/2)), (0, 0))
+    return util.pad(image, padding, 'constant')
+
 
 if __name__ == '__main__':
-    img = io.imread('img/Kropka.jpg')
+    img = io.imread('img/CT_ScoutView-large.jpg')
     img = color.rgb2gray(img)
+    img = to_square(img)
+
     plt.imshow(img)
     plt.show()
 
